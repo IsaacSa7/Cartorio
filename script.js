7b66a93f4b1b228ea6c44e787780ad90785b2c1d
isaac <isaac.sa.silva08@aluno.ifce.edu.br>
2023-06-16 15:37:27 -0300

    consegui conectar js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//botoes
+const btn_salvar = document.getElementById("btn_salvar")
+const btn_pesquisar = document.getElementById("btn_pesquisar")
+
+//vetor de certidoes
+const vetor_certidao = []
+
+
 class Certidao{
     constructor(nome, cpf, matricula, dataNasc, data, hora, naturalidade, muniRegistro, muniNasc, sexo, filiacao, avos, gemeos, nomeMGemeos, dataReg, dnv, anotacoes){
         this.nome = nome
@@ -92,63 +100,74 @@ class Certidao{
 //exibe dados
 let exibeDados = (x) => {
     return `
-<b>Nome : </b> ${x.getNome()}
-<b>Nome : </b> ${x.getNome()}
-<b>Nome : </b> ${x.getCpf()}
-<b>Nome : </b> ${x.getMatricula()}
-<b>Nome : </b> ${x.getDataNasc()}
-<b>Nome : </b> ${x.getData()}
-<b>Nome : </b> ${x.getHora()}
-<b>Nome : </b> ${x.getNaturalidade()}
-<b>Nome : </b> ${x.getMuniRegistro()}
-<b>Nome : </b> ${x.getMuniNasc()}
-<b>Nome : </b> ${x.getSexo()}
-<b>Nome : </b> ${x.getFiliacao()}
-<b>Nome : </b> ${x.getAvos()}
-<b>Nome : </b> ${x.getGemeos()}
-<b>Nome : </b> ${x.getNomeMGemeos()}
-<b>Nome : </b> ${x.getDataReg()}
-<b>Nome : </b> ${x.getDnv()}
-<b>Nome : </b> ${x.getAnotacoes()}
-    `
+    <b>Nome : </b> ${x.nome}<br>
+    <b>CPF : </b> ${x.cpf}<br>
+    <b>Matrícula : </b> ${x.matricula}<br>
+    <b>Data de Nascimento : </b> ${x.dataNasc}<br>
+    <b>Data : </b> ${x.data}<br>
+    <b>Hora : </b> ${x.hora}<br>
+    <b>Naturalidade : </b> ${x.naturalidade}<br>
+    <b>Município : </b> ${x.muniRegistro}<br>
+    <b>Município de Nascimento : </b> ${x.muniNasc}<br>
+    <b>Sexo : </b> ${x.sexo}<br>
+    <b>Filiação : </b> ${x.filiacao}<br>
+    <b>Avos : </b> ${x.avos}<br>
+    <b>Gemeos : </b> ${x.gemeos}<br>
+    <b>Nome dos Gemeos: </b> ${x.nomeMGemeos}<br>
+    <b>Data de Registro : </b> ${x.dataReg}<br>
+    <b>DNV: </b> ${x.dnv}<br>
+    <b>Anotações : </b> ${x.anotacoes}<br>
+`
 }
 
-//vetor de certidoes
-let vetor_certidao = []
-
-//cadastra
-let cadastra = (x) =>{
-    //pega variaveis
-    let nome = "nome"
-    let cpf = "cpf"
-    let matricula = x
-    let dataNasc = "dataNasc"
-    let data = "data"
-    let hora = "hora"
-    let naturalidade = "naturalidade"
-    let muniRegistro =  "muniRegistro"
-    let muniNasc = "muniNasc"
-    let sexo = "sexo"
-    let filiacao = "filiacao"
-    let avos = "avos"
-    let gemeos = "gemeos"
-    let nomeMGemeos = "nomeMGemeos"
-    let dataReg =  "dataReg"
-    let dnv = "dnv"
-    let anotacoes = "anotacoes"
+//
+let salvar = () =>{
+    let nome = document.getElementById("nome").value
+    let cpf = document.getElementById("cpf").value
+    let matricula = document.getElementById("matricula").value
+    let dataNasc = document.getElementById("data-nascimento").value
+    let data = document.getElementById("data").value
+    let hora = document.getElementById("hora-nascimento").value
+    let naturalidade = document.getElementById("naturalidade").value
+    let muniRegistro =  document.getElementById("municipio").value
+    let muniNasc = document.getElementById("municipio").value
+    let sexo = document.getElementById("sexo").value
+    let filiacao = document.getElementById("filiacao").value
+    let avos = document.getElementById("avos").value
+    let gemeos = document.getElementById("gemeos").value
+    let nomeMGemeos = document.getElementById("nome-gemeos").value
+    let dataReg =  document.getElementById("data-registro").value
+    let dnv = document.getElementById("dnv").value
+    let anotacoes = document.getElementById("averbacoes").value
 
     let c = new Certidao(nome, cpf, matricula, dataNasc, data, hora, naturalidade, muniRegistro, muniNasc, sexo, filiacao, avos, gemeos, nomeMGemeos, dataReg, dnv, anotacoes)
-
     vetor_certidao.push(c)
+
+    alert("CERTIDAO CADASTRADA COM SUCESSO")
+
+    sessionStorage.setItem('vetor_certidao', JSON.stringify(vetor_certidao))
 }
 
-//pesquisa
-let pesquisa = (v) =>{
-    for(let i = 0; i < (vetor_certidao.length); i++){
-        if(vetor_certidao[i].getMatricula() == v){
-            return vetor_certidao[i]
+let pesquisar = () =>{
+    v = document.getElementById("entrada").value
+    
+    let vetor = JSON.parse(sessionStorage.getItem('vetor_certidao'))
+    
+    for(let i = 0; i < vetor.length; i++){
+        if(vetor[i].matricula == v){
+            const saida = document.getElementById("saida")
+            saida.innerHTML = (exibeDados(vetor[i]))
+        } else {
+            alert("ERRO: CERTIDAO NAO ENCONTRADA")
         }
     }
+}
 
-    return "Certidao nao encontrada"
+let limpar = () =>{
+    const saida = document.getElementById("saida")
+    saida.innerHTML = ''
+}
+
+let naoFunciona = () =>{
+    alert("ESSA FUNCIONALIDADE AINDA NÃO ESTÁ DISPONÍVEL NESTA VERSÃO!")
 }
